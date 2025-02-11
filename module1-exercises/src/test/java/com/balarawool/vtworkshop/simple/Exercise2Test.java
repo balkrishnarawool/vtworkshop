9963204ee5692cce1265da0615356b503146c8b5
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2025-02-11 22:49:31 +0100

    Solution for Exercise 2

--- module1-exercises/src/test/java/com/balarawool/vtworkshop/simple/Exercise2Test.java
+++ module1-exercises/src/test/java/com/balarawool/vtworkshop/simple/Exercise2Test.java
@@ -12,15 +12,15 @@ public class Exercise2Test {
     // Change the threads to virtual threads and you can increase the number to 1 million or even beyond.
     public static void main(String[] args) throws InterruptedException {
         var list = new ArrayList<Thread>();
-        for (int i = 0; i < 1_000; i++) {
-            //var t = Thread.....unstarted(() -> {
-            //    try {
-            //        Thread.sleep(1_000L);
-            //    } catch (InterruptedException e) {
-            //        throw new RuntimeException(e);
-            //    }
-            //});
-            //list.add(t);
+        for (int i = 0; i < 10_000_000; i++) {
+            var t = Thread.ofVirtual().unstarted(() -> {
+                try {
+                    Thread.sleep(1_000L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            list.add(t);
         }
         list.forEach(Thread::start);
     }
