45003c714eddd6fcdbd1e5c1fda15d5022cb5e98
Balkrishna Rawool <balkrishna.rawool@gmail.com>
2025-02-11 22:41:53 +0100

    Solution for Exercise 1

--- module1-exercises/src/test/java/com/balarawool/vtworkshop/simple/Exercise1Test.java
+++ module1-exercises/src/test/java/com/balarawool/vtworkshop/simple/Exercise1Test.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -13,26 +14,26 @@ public class Exercise1Test {
     // TODO Exercise 1.1: Create platform thread that prints current-thread (Thread.currentThread()).
     @Test
     public void createPlatformThread() {
-        fail("Exercise not completed");
-        //Thread t =
-        //assertFalse(t.isVirtual());
+        Thread t = Thread.ofPlatform().start(() -> System.out.println(Thread.currentThread()));
+        assertFalse(t.isVirtual());
     }
 
     // TODO Exercise 1.2: Create virtual thread that prints current thread.
     @Test
     public void createVirtualThread() {
-        fail("Exercise not completed");
-        //Thread t =
-        //assertTrue(t.isVirtual());
+        Thread t = Thread.ofVirtual().start(() -> System.out.println(Thread.currentThread()));
+        assertTrue(t.isVirtual());
     }
 
     // TODO Exercise 1.3: Create an ExecutorService that creates virtual threads on each request and
     //  use it to create 10 virtual-threads.
     @Test
     public void createVirtualThreadsUsingExecutor() {
-        fail("Exercise not completed");
-        //try (var executor = ) {
-        //    assertTrue(executor instanceof ExecutorService);
-        //}
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (int i = 0; i < 10; i++) {
+                executor.submit(() -> System.out.println(Thread.currentThread()));
+            }
+            assertTrue(executor instanceof ExecutorService);
+        }
     }
 }
